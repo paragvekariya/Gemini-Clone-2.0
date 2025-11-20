9470dc5bc3c2a5a2a1f2deb5913335406f08ddef
Parag Vekariya <154518531+paragvekariya@users.noreply.github.com>
2025-11-20 17:20:55 +0530

    Update script.js

--- script/style/script.js
+++ script/style/script.js
@@ -4,3 +4,8 @@ const promptform = document.querySelector(".prompt-form");
 const chatContainer = document.querySelector(".chats-container");
 const promptInput = promptform.querySelector(".prompt-input");
 const fileInput = promptform.querySelector("#file-input");
+const fileUploadWrapper = promptform.querySelector(".file-upload-wrapper");
+const themeToggleBtn = document.querySelector("#theme-toggle-btn");
+
+
+//API Copnfiguration
