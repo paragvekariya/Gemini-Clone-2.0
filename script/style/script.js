2fa0c487382ae3bd04c590347a49ba9c296cdb5f
Parag Vekariya <154518531+paragvekariya@users.noreply.github.com>
2025-11-20 17:15:29 +0530

    Update script.js

--- script/style/script.js
+++ script/style/script.js
@@ -1,1 +1,6 @@
 // dom elements
+const container = document.querySelector(".container");
+const promptform = document.querySelector(".prompt-form");
+const chatContainer = document.querySelector(".chats-container");
+const promptInput = promptform.querySelector(".prompt-input");
+const fileInput = promptform.querySelector("#file-input");
